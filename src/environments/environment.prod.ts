f528b85399909826e1f898b0b3c2f1c84aab2f22
jiao0522 <zhangsoon930522@gmail.com>
2022-09-13 23:16:56 +0800

    update servel api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,8 +7,8 @@ export const environment = {
     baseUrl: './',
     refreshTokenEnabled: true,
     refreshTokenType: 'auth-refresh',
-    serverUrl: 'https://beta2.orderbot.jp/',
-    apiUrl: 'https://beta2.orderbot.jp/api/'
+    serverUrl: 'https://beta1.orderbot.jp/',
+    apiUrl: 'https://beta1.orderbot.jp/api/'
     // serverUrl: 'http://localhost:3000/',
     // apiUrl: 'http://localhost:3000/api/'
   }
