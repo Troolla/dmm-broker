e1e314b100df24ad16a8ac7d70e19811bba4845b
jiao0522 <zhangsoon930522@gmail.com>
2022-09-30 17:51:47 +0800

    update uat url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,8 +7,8 @@ export const environment = {
     baseUrl: './',
     refreshTokenEnabled: true,
     refreshTokenType: 'auth-refresh',
-    serverUrl: 'https://beta1.orderbot.jp/',
-    apiUrl: 'https://beta1.orderbot.jp/api/'
+     serverUrl: 'https://orderbot.jp/',
+     apiUrl: 'https://orderbot.jp/api/'
     // serverUrl: 'http://localhost:3000/',
     // apiUrl: 'http://localhost:3000/api/'
   }
